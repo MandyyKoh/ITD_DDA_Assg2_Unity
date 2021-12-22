23163448aad99d9d3d8ed33e0d33795969d06ca0
EvanTalavera <73120338+EvanTalavera@users.noreply.github.com>
2021-12-22 21:59:51 +0800

    Clue implementation
    Clues have now been placed into their proper places and spawn properly.
    (This only applies for Operation Barbarossa, Operation Sea Lion pending implementation.)

--- Assets/Scripts/WireScript.cs
+++ Assets/Scripts/WireScript.cs
@@ -8,6 +8,8 @@ public class WireScript : MonoBehaviour
     public Light indicatorLight;
 
     public Animator safeDoorAnimator;
+    public GameObject blankDocument;
+    public GameObject clueDocument;
     public void AddConnectedWire() 
     {
         connectedWires += 1;
@@ -20,6 +22,8 @@ public class WireScript : MonoBehaviour
         if (connectedWires >= 4) 
         {
             indicatorLight.enabled = true;
+            blankDocument.SetActive(false);
+            clueDocument.SetActive(true);
             DoorHandle.instance.electricalSolved = true;
             Stopwatch.instance.StopStopwatch("electrical");
             safeDoorAnimator.SetBool("ElectricalDone", true);
